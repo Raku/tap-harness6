1bc6566e2e8f1cd04416a0283329752718f43c47
Leon Timmermans <fawaka@gmail.com>
2016-01-06 11:34:05 +0100

    Move Actions class into Grammar

--- lib/TAP.pm
+++ lib/TAP.pm
@@ -163,14 +163,6 @@ class Aggregator {
 }
 
 grammar Grammar {
-	method parse {
-		my $*tap-indent = 0;
-		callsame;
-	}
-	method subparse($, *%) {
-		my $*tap-indent = 0;
-		callsame;
-	}
 	token TOP { ^ <line>+ $ }
 	token sp { <[\s] - [\n]> }
 	token num { <[0..9]>+ }
@@ -217,57 +209,65 @@ grammar Grammar {
 	token unknown {
 		\N*
 	}
-}
-class Action {
-	method TOP($/) {
-		make @<line>».made;
-	}
-	method line($/) {
-		make $/.values[0].made;
-	}
-	method plan($/) {
-		my %args = :raw(~$/), :tests(+$<count>);
-		if $<directive> {
-			%args<skip-all explanation> = True, $<explanation>;
+	class Actions {
+		method TOP($/) {
+			make @<line>».made;
 		}
-		make TAP::Plan.new(|%args);
+		method line($/) {
+			make $/.values[0].made;
+		}
+		method plan($/) {
+			my %args = :raw(~$/), :tests(+$<count>);
+			if $<directive> {
+				%args<skip-all explanation> = True, $<explanation>;
+			}
+			make TAP::Plan.new(|%args);
+		}
+		method description($/) {
+			make ~$/.subst(/\\('#'|'\\')/, { $_[0] }, :g)
+		}
+		method !make_test($/) {
+			my %args = (:ok($<nok> eq ''));
+			%args<number> = $<num> ?? +$<num> !! Int;
+			%args<description> = $<description>.made if $<description>;
+			%args<directive> = $<directive> ?? TAP::Directive::{~$<directive>.substr(0,4).tclc} !! TAP::No-Directive;
+			%args<explanation> = ~$<explanation> if $<explanation>;
+			return %args;
+		}
+		method test($/) {
+			make TAP::Test.new(:raw(~$/), |self!make_test($/));
+		}
+		method bailout($/) {
+			make TAP::Bailout.new(:raw(~$/), :explanation($<explanation> ?? ~$<explanation> !! Str));
+		}
+		method version($/) {
+			make TAP::Version.new(:raw(~$/), :version(+$<version>));
+		}
+		method comment($/) {
+			make TAP::Comment.new(:raw(~$/), :comment(~$<comment>));
+		}
+		method yaml($/) {
+			my $serialized = $<yaml-line>.join('');
+			my $deserialized = try (require YAMLish) ?? YAMLish::load-yaml("---\n$serialized...") !! Any;
+			make TAP::YAML.new(:raw(~$/), :$serialized, :$deserialized);
+		}
+		method sub-entry($/) {
+			make $/.values[0].made;
+		}
+		method sub-test($/) {
+			make TAP::Sub-Test.new(:raw(~$/), :entries(@<sub-entry>».made), |self!make_test($<test>));
+		}
+		method unknown($/) {
+			make TAP::Unknown.new(:raw(~$/));
+		}
 	}
-	method description($/) {
-		make ~$/.subst(/\\('#'|'\\')/, { $_[0] }, :g)
+	method parse(|c) {
+		my $*tap-indent = 0;
+		nextwith(:actions(Actions), |c);
 	}
-	method !make_test($/) {
-		my %args = (:ok($<nok> eq ''));
-		%args<number> = $<num>.defined ?? +$<num> !! Int;
-		%args<description> = $<description>.made if $<description>;
-		%args<directive> = $<directive> ?? TAP::Directive::{~$<directive>.substr(0,4).tclc} !! TAP::No-Directive;
-		%args<explanation> = ~$<explanation> if $<explanation>;
-		return %args;
-	}
-	method test($/) {
-		make TAP::Test.new(:raw(~$/), |self!make_test($/));
-	}
-	method bailout($/) {
-		make TAP::Bailout.new(:raw(~$/), :explanation($<explanation> ?? ~$<explanation> !! Str));
-	}
-	method version($/) {
-		make TAP::Version.new(:raw(~$/), :version(+$<version>));
-	}
-	method comment($/) {
-		make TAP::Comment.new(:raw(~$/), :comment(~$<comment>));
-	}
-	method yaml($/) {
-		my $serialized = $<yaml-line>.join('');
-		my $deserialized = try (require YAMLish) ?? YAMLish::load-yaml("---\n$serialized...") !! Any;
-		make TAP::YAML.new(:raw(~$/), :$serialized, :$deserialized);
-	}
-	method sub-entry($/) {
-		make $/.values[0].made;
-	}
-	method sub-test($/) {
-		make TAP::Sub-Test.new(:raw(~$/), :entries(@<sub-entry>».made), |self!make_test($<test>));
-	}
-	method unknown($/) {
-		make TAP::Unknown.new(:raw(~$/));
+	method subparse(|c) {
+		my $*tap-indent = 0;
+		nextwith(:actions(Actions), |c);
 	}
 }
 
@@ -275,11 +275,10 @@ class Parser {
 	has Str $!buffer = '';
 	has TAP::Entry::Handler @!handlers;
 	has Grammar $!grammar = Grammar.new;
-	has Action $!actions = Action.new;
 	submethod BUILD(:@!handlers) { }
 	method add-data(Str $data) {
 		$!buffer ~= $data;
-		while ($!grammar.subparse($!buffer, :$!actions)) -> $match {
+		while ($!grammar.subparse($!buffer)) -> $match {
 			$!buffer.=substr($match.to);
 			for @($match.made) -> $result {
 				@!handlers».handle-entry($result);
