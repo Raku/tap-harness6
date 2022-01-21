4f75175bca843510543999b98174c8ad2ca08f38
Leon Timmermans <fawaka@gmail.com>
2022-01-21 23:28:48 +0100

    Allow SourceHandler::Raku's incdirs arguments to be Str or IO::Path

--- lib/TAP.pm
+++ lib/TAP.pm
@@ -890,7 +890,7 @@ class Harness {
     }
     class SourceHandler::Raku does SourceHandler::Proc {
         has Str:D $.path = $*EXECUTABLE.absolute;
-        has Str @.incdirs;
+        has Str(IO) @.incdirs;
         method make-source(Str:D $name, Any:D :$err, IO::Path:D :$cwd, :@include-dirs = (), *%) {
             my @dirs = map { normalize-path($_, $cwd) }, |@include-dirs, |@!incdirs;
             my @args = |@dirs.map("-I" ~ *), $name;
